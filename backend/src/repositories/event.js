6c53493afaea1be7ae4f6f461ff6a3727133cc0c
Hugo-Fabresse <146661032+Hugo-Fabresse@users.noreply.github.com>
2025-09-07 22:27:03 +0200

    feat(events): implement CRUD operations for events and add error hand… (#52)
    * feat(events): implement CRUD operations for events and add error handling
    
    * fix(events): correct EventService import and update response codes in event handling

---
+++ backend/src/repositories/event.js
@@ -0,0 +1,44 @@
+const prisma = require('@config/prisma');
+
+class Event {
+    static countAll() {
+        return prisma.events.count();
+    }
+
+    static getAllPaginated({ skip, take }) {
+        return prisma.events.findMany({
+            skip,
+            take,
+            orderBy: {
+                created_at: 'desc'
+            }
+        });
+    }
+
+    static async getById(id) {
+        return prisma.events.findUnique({
+            where: { id }
+        });
+    }
+
+    static async create(data) {
+        return prisma.events.create({
+            data
+        });
+    }
+
+    static async update(id, data) {
+        return prisma.events.update({
+            where: { id },
+            data
+        });
+    }
+
+    static async delete(id) {
+        return prisma.events.delete({
+            where: { id }
+        });
+    }
+}
+
+module.exports = Event;
