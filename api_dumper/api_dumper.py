bd7cffe25ff442929304e346c1e333c31e59f671
Morgan <morgan.guichard@epitech.eu>
2025-09-08 17:47:17 +0200

    fix: debug test resolved

--- api_dumper/api_dumper.py
+++ api_dumper/api_dumper.py
@@ -13,7 +13,6 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_HOST = "localhost"
 DB_PORT = int(os.getenv("DB_PORT", 5432))
 
 # --- Config ---
