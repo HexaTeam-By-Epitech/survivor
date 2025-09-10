dd3b810823ea1c66de7b7e64433b05eb4b5a76e4
Morgan <morgan.guichard@epitech.eu>
2025-09-10 17:49:33 +0200

    fix: now db_init scripts are compatible for both Docker AND local machines

--- api_dumper/init_and_dump.sh
+++ api_dumper/init_and_dump.sh
@@ -1,5 +1,8 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -euo pipefail
+
+# Reinitialize database
+./reset_db.sh
 
 # Initialize database
 ./init_db.sh
@@ -9,7 +12,7 @@ python3 -m venv venv || true
 source venv/bin/activate
 
 # Install system deps only if needed
-# dpkg -s libpq-dev python3-dev gcc >/dev/null 2>&1 || sudo apt update && sudo apt install libpq-dev python3-dev gcc -y
+dpkg -s libpq-dev python3-dev gcc >/dev/null 2>&1 || sudo apt update && sudo apt install libpq-dev python3-dev gcc -y
 
 # Install Python deps
 pip install --upgrade --no-cache-dir -r requirements.txt
